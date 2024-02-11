a1f02e2c3d34641cbb242b8cc4d7443e13994ebc
Alexandra Asanache <ale.asanache@gmail.com>
2024-02-11 16:11:31 +0000

    Added function to check the time based on past, present, future

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,45 @@ $(function() {
         localStorage.setItem(hourBlock, textInput);
     });
     
+    //Variables for HTML elements to display the date and time
+    const displayDateEl = $('#currentDay');
+    const displayTimeEl = $('#currentTime')
+
+     //compare time block hour with the present hour
+     function checkTime() {
+        //get the present hour with DayJs
+        const presentHour = dayjs().format('H');
+        // console.log(presentHour);
+
+        //check set hours in each time block
+        $(".time-block").each(function(){
+            const hourID= parseInt($(this).attr('id').split("hour-")[1]);
+            // console.log(hourID);
+
+        //add relevant classes based on whether the time block is in the present, past, or future hour.
+
+        if (hourID < presentHour) {
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+            $(this).addClass('past');
+
+
+        } else if (hourID === presentHour) {
+            $(this).removeClass('past');
+            $(this).removeClass('future');
+            $(this).addClass('present');
+        } else {
+            $(this).removeClass('past');
+            $(this).removeClass('present');
+            $(this).addClass('future');
+        }
+        });
+    }
+
+    //call checkTime every minute so it stays up to date
+    checkTime();
+    setInterval(checkTime, 60000);
+
+
+
     })
